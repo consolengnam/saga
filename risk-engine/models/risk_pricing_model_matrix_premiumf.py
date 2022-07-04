8132ab9f3d6664d147e832f6f764453d0edab308
ulrichvianney <vianneykokwe@gmail.com>
2022-07-04 05:04:50 -0700

    modification de la vue premium f

--- risk-engine/models/risk_pricing_model_matrix_premiumf.py
+++ risk-engine/models/risk_pricing_model_matrix_premiumf.py
@@ -10,7 +10,8 @@ class RiskPricingModelMatrixPremium(models.Model):
     _name = 'risk.premium.f.curves'
     _description = 'Pricing model matrix - premium F'
 
-    period = fields.Integer('period', required=True),
+    period = fields.Integer('period', required=True)
     rate = fields.Float('rate', digits=(12, 6), required=True)
     status = fields.Selection([('1', 'Active'), ('2', 'Inactive')], required=True, default='1')
+
     # conversion_factor_default_rate = fields.Many2one('risk.pricing.model.constants', required=True)
