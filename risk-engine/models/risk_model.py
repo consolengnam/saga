f5d38230a3a98cea7d366f6964d7cd790da1e03c
consolengnam <consolengnam@gmail.com>
2022-07-05 17:36:31 +0100

    Activation of Security

--- risk-engine/models/risk_model.py
+++ risk-engine/models/risk_model.py
@@ -14,8 +14,12 @@ class RiskModels(models.Model):
     model_name = fields.Char(string='Model Name', required=True)
     model_description = fields.Text(string='Model Description', required=True)
     status = fields.Selection([('1', 'Active'), ('2', 'Inactive')], required=True, default='1')
-    type_id = fields.Many2one('risk.models.types', string='Type', required=True)
+    # type_id = fields.Many2one('risk.models.types', string='Type', required=True)
     risk_model_factors = fields.One2many('risk.model.factor', 'risk_model_id', required=True)
+    type_risk = fields.Selection([
+        ('country_risk_models', 'Country Risk Models'), ('lender_risk_models', 'Lender Risk Models'),
+        ('borrower_risk_models', 'Borrower Risk Models'), ('transaction_risk_models', 'Transaction Risk Model')],
+        string='Type', required=True, default='country_risk_models')
 
 
 class RiskModelTypes(models.Model):
@@ -25,5 +29,5 @@ class RiskModelTypes(models.Model):
 
     type = fields.Char(string='Type', required=True)
     description = fields.Text(string='Description', required=True)
-    risk_models = fields.One2many('risk.models', 'type_id', string='Risks')
+    #risk_models = fields.One2many('risk.models', 'type_id', string='Risks')
     code = fields.Char(string='Type', required=True)
